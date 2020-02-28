a4c1a4dc427e8735fdd2c39c4af178cc3a3843bc
Edson Cavalcanti <edson.horacavalcanti@gmail.com>
2020-02-28 06:59:25 -0300

    Aula 01: Começando com Spring MVC

---
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -0,0 +1,33 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.com.casadocodigo.loja.controllers.HomeController;
+
+/**
+ * 
+ * @author Edson Cavalcanti
+ * - Essa classe tem a finalidade de informar ao Spring o diretório onde estaram as views do projeto.
+ * - Objetivo de criar a pasta WEB-INF é de proteger os arquivos para não serem acessados diretamente sem passar pelo controller.
+ * - InternalResourceViewResolver = Resolvedor Interno de Recursos View que ajuda o Spring a encontrar as views.
+ * - @Bean é uma anotação de configuração para que o Spring MVC possa gerenciar no método
+ * - @ComponentScan recebe um array de classes de onde o Spring MVC pode extrair de forma automatica os outros controllers.
+ * - @EnableWebMvc anotação para habilitar o recurso Web MVC do Spring MVC.
+ */
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {HomeController.class}) 
+public class AppWebConfiguration {
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+	    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	    resolver.setPrefix("/WEB-INF/views/"); // Caminho das views
+	    resolver.setSuffix(".jsp"); // Adiciona extensão dos arquivos de view
+	    return resolver;
+	}
+	
+}
---
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMvc.java
@@ -0,0 +1,29 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * 
+ * @author Edson Cavalcanti
+ * - Classe de configuração do Servlet do Spring MVC para que ele atenda as requisições recebidas pelo servidor.
+ *
+ */
+
+public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {AppWebConfiguration.class}; // Referencia a classe de configuração.
+	}
+
+	@Override
+	protected String[] getServletMappings() { // Array com os mapeamentos que o Servlet irá atender.
+		return new String[] {"/"}; // Dessa forma o Servlet atenderá as requisições a partir da raiz do projeto.
+	}
+
+}
